e6f4fef258883ec4745e1969104327496e43c90b
mustafatawab <engrmustafatawab05@gmail.com>
2025-05-27 17:30:49 +0500

    static and instance variables

--- data_classes.py
+++ data_classes.py
@@ -3,11 +3,20 @@ from typing import ClassVar
 
 @dataclass
 class American:
+  #static Variable because English use all the americans
+  national_language : ClassVar[str] = 'English'  # the Type of the language is Generic
+
+  #static varible because Humberger eat all americans
+  national_food : ClassVar[str] = "Humberger"
+
+  #static variable
+  normal_temperature  :ClassVar[float] = 98.6
+
+  #instance varibale because this is for individual
   name : str
   age : int
   weight : float
-
-  language : ClassVar[str] = 'English'  # the Type of the language is Generic
+  liked_food : str  
 
   def eat(self):
     print(f"{self.name} is eating")
@@ -16,11 +25,11 @@ class American:
     print(f"{self.name} is sleeping")
 
   def speak(self):
-    print(f"{self.name} is speaking {American.language}")
+    print(f"{self.name} is speaking {American.national_language}")
   
   @staticmethod
   def countryLanguage():
-    print(f"{American.language} is the language of America")
+    print(f"{American.national_language} is the language of America")
 
 
 person = American("John" , 24 , 56.6)
