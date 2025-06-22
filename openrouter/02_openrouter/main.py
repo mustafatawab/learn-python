1061a6ba7bbf65deff4a6725f21a0a36f8ea50f0
mustafatawab <engrmustafatawab05@gmail.com>
2025-06-22 15:54:48 +0500

    openrouter with chainlit and uv

--- openrouter/02_openrouter/main.py
+++ openrouter/02_openrouter/main.py
@@ -15,28 +15,53 @@ OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 
 
-response = requests.post(
-  url=f"{BASE_URL}/chat/completions",
-  headers={
-    "Authorization": f"Bearer {OPENROUTER_API_KEY}",
-  },
-  data=json.dumps({
-    "model": MODEL,
-    "messages": [
-      {
-        "role": "user",
-        "content": "what do you think about the my personal software agency. The name is Farsight Systems. "
-      }
-    ]
-  })
-)
+
+
+@cl.on_chat_start
+async def handle_chat_start() -> None:
+    cl.user_session.set("history" , []),
+    await cl.Message(content="I am general assistant").send()
 
 
 @cl.on_message
-async def send_message(msg: cl.Message) -> None:
-    await cl.Message(
-        content=f"I received {msg.content} from the user"
-    ).send()
+async def send_message(message: cl.Message) -> None:
+    history = cl.user_session.get("history")
+
+    msg = cl.Message(content="")
+    await msg.send()
+
+    history.append({
+        "role" : "user",
+        "content" : message.content
+    })
+
+    print(history)
+    response = requests.post(
+      url=f"{BASE_URL}/chat/completions",
+      headers={
+        "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+      },
+      data=json.dumps({
+        "model": MODEL,
+        "messages": [
+          {
+            "role": "user",
+            "content": history
+          }
+        ]
+      })
+    )
+
+    # Input must have atleast one token
+    data  = response.json()
+    print(data)
+
+    history.append({
+        "role" : "assistant",
+        "content" : data['choices'][0]['message']['content']
+    })
+
+    cl.user_session.set("history" , history)
 
 
 
