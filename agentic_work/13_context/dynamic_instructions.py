ab8c3e233c0a2df8ebe3d2a92362569b0b7de9da
mustafatawab <engrmustafatawab05@gmail.com>
2025-08-05 21:07:07 +0500

    dynamic instructions

---
+++ agentic_work/13_context/dynamic_instructions.py
@@ -0,0 +1,47 @@
+import asyncio
+from multiprocessing import context
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, set_tracing_disabled, RunContextWrapper, function_tool
+from dotenv import load_dotenv, find_dotenv
+import os
+from agents.run import RunConfig
+from dataclasses import dataclass
+
+
+load_dotenv()
+
+gemini_key = os.environ.get("GEMINI_API_KEY")
+
+if not gemini_key:
+    raise ValueError("Please set gemini api 🔑 ")
+
+@dataclass
+class User:
+    username:str
+    email: str
+
+
+@function_tool
+def search(context: RunContextWrapper[User] , query : str) -> str:
+    print(context.context)
+    return ""
+
+set_tracing_disabled(True)
+
+client = AsyncOpenAI(api_key=gemini_key , base_url='https://generativelanguage.googleapis.com/v1beta/openai/')
+
+llm_model = OpenAIChatCompletionsModel(openai_client=client, model="gemini-2.0-flash")
+
+config = RunConfig(model=llm_model, model_provider=client)
+
+def special_prompt(local_context: RunContextWrapper[User] , agent: Agent):
+    print(f"{local_context.context} \n agent {agent} \n")
+    return f"You are Math Expert. Your name is {local_context.context.username} and agent name is {agent.name}"
+
+
+agent = Agent(name="assitant" , instructions=special_prompt, tools=[search])
+
+user = User(username='Mustafa Tawab' , email='tawab05@gmail.com')
+
+result = Runner.run_sync(agent, 'what is your and your agent name', run_config=config, context=user)
+
+print(result.final_output)
