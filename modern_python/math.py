f6ed34ba0b7c6fd942e8d4d000dab89b23f59ae3
mustafatawab <engrmustafatawab05@gmail.com>
2025-12-29 22:13:10 +0500

    Math module in python

---
+++ modern_python/math.py
@@ -0,0 +1,13 @@
+import math
+
+number1: int = 5
+
+print("\nSquare Root ", math.sqrt(number1))
+
+print("\nCeil " ,math.ceil(number1))
+
+print("\nFloor" ,math.floor(number1))
+
+print("\nFactorial" ,math.factorial(number1))
+
+print("\nPower ",math.pow(number1 , 2))
